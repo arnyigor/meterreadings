88324e2be64ebd7750372426d248f8960eeedb63
arnyigor <arnyigor@gmail.com>
2021-03-19 02:33:42 +0300

    base read xml

--- dataimporter/src/main/java/com/arny/dataimporter/data/xml/XmlImporterImpl.kt
+++ dataimporter/src/main/java/com/arny/dataimporter/data/xml/XmlImporterImpl.kt
@@ -2,12 +2,27 @@ package com.arny.dataimporter.data.xml
 
 import android.content.Context
 import com.arny.dataimporter.R
+import org.xmlpull.v1.XmlPullParser
 import javax.inject.Inject
 
 class XmlImporterImpl @Inject constructor(
-  private val context: Context
+    private val context: Context
 ) : DataImporter {
-  override fun importData(): String {
-     return "Импорт данных ${context.getString(R.string.test_data)}"
-  }
+    override fun importData(): String {
+        val parser = context.resources.getXml(R.xml.data)
+        val toString = StringBuilder().apply {
+            while (parser.eventType != XmlPullParser.END_DOCUMENT) {
+                if (parser.eventType == XmlPullParser.START_TAG
+                    && parser.name.equals("DATA")
+                ) {
+                    for (i in 0 until parser.attributeCount) {
+                        val attributeName = parser.getAttributeName(i)
+                        val value = parser.getAttributeValue(i)
+                    }
+                }
+                parser.next();
+            }
+        }.toString()
+        return "Импорт данных $toString"
+    }
 }
